b3af73148fe7363fdfe7b79966973799dcb895bd
jroebroek <roebroek.jasper@gmail.com>
2025-11-20 13:56:41 +0100

    Fix version number not found

--- pygeodata/__init__.py
+++ pygeodata/__init__.py
@@ -2,8 +2,6 @@ from pygeodata.base import load, process
 from pygeodata.config import set_config
 from pygeodata.loader import DataLoader
 
-__version__ = '0.0.2'
-
 __all__ = [
     'DataLoader',
     'load',
